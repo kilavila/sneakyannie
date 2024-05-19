5b14a1eaafa8e03b85e4cbac7ceac03b5dfec73a
kilavila <christer.kilavik@protonmail.com>
2024-05-20 00:23:12 +0200

    feat(cmd): working on cards and search

--- js/components/commands.component.js
+++ js/components/commands.component.js
@@ -25,12 +25,21 @@ class CommandsComponent extends HTMLElement {
 			.listeners();
 	}
 
+	cmdIcon(type) {
+		if (type === 'highlighted') return 'nf-md-star';
+		if (type === 'sound') return 'nf-md-music';
+		if (type === 'video') return 'nf-md-video';
+		if (type === 'other') return 'nf-md-hexagon_outline';
+	}
+
 	renderCommand(cmd) {
 		return `
-			<li class="list-item ${cmd.type === 'highlighted' ? 'highlight' : ''}">
+			<li title="Copy ${cmd.keyword} to clipboard" data-cmd="${cmd.keyword}" class="list-item ${cmd.type === 'highlighted' ? 'highlight' : ''}">
 				<div class="list-item-header">
 					<div class="keyword">${cmd.keyword}</div>
-					<div class="type">${cmd.type}</div>
+					<div class="type">
+						<i class="nf ${this.cmdIcon(cmd.type)}"></i>
+					</div>
 				</div>
 				<div class="description">${cmd.description}</div>
 			</li>
@@ -38,6 +47,7 @@ class CommandsComponent extends HTMLElement {
 	}
 
 	render() {
+		this.id = 'commands';
 		this.innerHTML = `
 			<h2>
 				Commands
@@ -51,15 +61,18 @@ class CommandsComponent extends HTMLElement {
 			</div>
 
 			<ul class="command-list">
-				${this.commands.map(cmd => this.renderCommand(cmd)).join('')}
+				${this.commands.slice(0, 6).map(cmd => this.renderCommand(cmd)).join('')}
+				<li class="cmd-expand-list">
+					View all commands
+					<i class="nf nf-fa-chevron_down"></i>
+				</li>
 			</ul>
 		`;
 
 		return this;
 	}
 
-	commandSearch = debounce((query) => {
-		const commandList = this.querySelector('.command-list');
+	commandSearch = debounce((commandList, query) => {
 		const filter = this.commands.filter(
 			cmd => cmd.keyword.toLowerCase().includes(query) ||
 				cmd.description.toLowerCase().includes(query) ||
@@ -68,9 +81,25 @@ class CommandsComponent extends HTMLElement {
 		commandList.innerHTML = filter.map(cmd => this.renderCommand(cmd)).join('');
 	}, 500);
 
+	viewAllCommands(commandList) {
+		commandList.innerHTML = commands.map(cmd => this.renderCommand(cmd)).join('');
+	}
+
 	listeners() {
+		const commandList = this.querySelector('.command-list');
+		const commandListItems = this.querySelectorAll('.list-item');
+		commandListItems.forEach(item => {
+			item.addEventListener('click', () => {
+				const keyword = item.getAttribute('data-cmd');
+				navigator.clipboard.writeText(keyword);
+			});
+		});
+
 		const searchField = this.querySelector('#searchField');
-		searchField.addEventListener('input', () => this.commandSearch(searchField.value.toLowerCase()));
+		searchField.addEventListener('input', () => this.commandSearch(commandList, searchField.value.toLowerCase()));
+
+		const expandCmds = this.querySelector('.cmd-expand-list');
+		expandCmds.addEventListener('click', () => this.viewAllCommands(commandList));
 
 		return this;
 	}
